ae76693e0bb36478826befd2fcf0c0b2abd88378
Arkadiusz4 <arekmika4@gmail.com>
2024-05-24 23:19:29 +0200

    Update Program.cs

--- CarFlex/Program.cs
+++ CarFlex/Program.cs
@@ -1,53 +1,16 @@
-// using CarFlex.Data;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.Extensions.DependencyInjection;
-//
-// var builder = WebApplication.CreateBuilder(args);
-// builder.Services.AddDbContext<CarFlexDbContext>(options =>
-//     options.UseSqlite(builder.Configuration.GetConnectionString("CarFlexDbContext") ??
-//                       throw new InvalidOperationException("Connection string 'CarFlexDbContext' not found.")));
-//
-// // Add services to the container.
-// builder.Services.AddControllersWithViews();
-//
-// var app = builder. Build();
-//
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-//
-// app.UseHttpsRedirection();
-// app.UseStaticFiles();
-//
-// app.UseRouting();
-//
-// app.UseAuthorization();
-//
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Home}/{action=Index}/{id?}");
-//
-// app.Run();
-
 using CarFlex.Data;
-using CarFlex.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure services
 builder.Services.AddDbContext<CarFlexDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("CarFlexDbContext") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("CarFlexDbContext") ??
+                      throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Seed the database
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -55,7 +18,6 @@ using (var scope = app.Services.CreateScope())
     DbInitializer.Initialize(context);
 }
 
-// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
